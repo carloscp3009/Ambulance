81efbbd4ae4be652923c61884c66a3beebf932fa
carlos <ccarlosa@uninorte.edu.co>
2019-05-23 21:42:35 -0500

    beds listed

--- Ambulance/src/routes/areas.js
+++ Ambulance/src/routes/areas.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const Areas = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
+router.get('/', isLoggedIn, async (req, res) => {
+  const areas = await Areas.query('SELECT * FROM areas WHERE id_hospital = ?', [
+    req.user.idhospital
+  ]);
+  res.render('areas/list', { areas });
+});
+
+// Add Areas  - - - - - - - - - - - - - - - - - - - - - - - - - - -
 router.get('/add', isLoggedIn, (req, res) => {
   res.render('areas/add');
 });
@@ -20,13 +28,7 @@ router.post('/add', isLoggedIn, async (req, res) => {
   res.redirect('/areas');
 });
 
-router.get('/', isLoggedIn, async (req, res) => {
-  const areas = await Areas.query('SELECT * FROM areas WHERE id_hospital = ?', [
-    req.user.idhospital
-  ]);
-  res.render('areas/list', { areas });
-});
-
+// Delete area - - - - - - - - - - - - - - - - - - - - - - - - - - -
 router.get('/delete/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
   await Areas.query('DELETE FROM areas WHERE id = ?', [id]);
@@ -34,6 +36,7 @@ router.get('/delete/:id', isLoggedIn, async (req, res) => {
   res.redirect('/areas');
 });
 
+// Edit Area - - - - - - - - - - - - - - - - - - - - - - - - - - -
 router.get('/edit/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const area = await Areas.query('SELECT * FROM areas WHERE id = ?', [id]);
@@ -51,4 +54,28 @@ router.post('/edit/:id', isLoggedIn, async (req, res) => {
   res.redirect('/areas');
 });
 
+// Details =====================================================
+router.get('/details/:id', isLoggedIn, async (req, res) => {
+  const { id } = req.params;
+  const beds = await Areas.query('SELECT * FROM beds WHERE id_area = ?', [id]);
+  console.log(beds);
+  res.render('areas/beds/details', { beds });
+});
+// add beds - - - - - - - - - - - - - - - - - - - - - - - - - -
+router.get('/add/details', isLoggedIn, (req, res) => {
+  res.render('areas/beds/add');
+});
+
+router.post('/add', isLoggedIn, async (req, res) => {
+  const { name, beds } = req.body;
+  const newArea = {
+    id: null,
+    name,
+    id_hospital: req.user.idhospital
+  };
+  await Areas.query('INSERT INTO areas set ?', [newArea]);
+  req.flash('success', 'Area saved successfully');
+  res.redirect('/areas');
+});
+//export module
 module.exports = router;
--- Ambulance/src/routes/index.js
+++ Ambulance/src/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('start');
+  res.render('index');
 });
 router.get('/s', (req, res) => {
-  res.render('index');
+  res.render('start');
 });
 module.exports = router;
