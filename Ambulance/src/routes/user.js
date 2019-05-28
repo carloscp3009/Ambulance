6487b3ebc78051adae7e4d044f17f5f44468532c
carlos <ccarlosa@uninorte.edu.co>
2019-05-28 10:50:02 -0500

    add usr ok

--- Ambulance/src/routes/user.js
+++ Ambulance/src/routes/user.js
@@ -17,6 +17,7 @@ router.get('/add', isLoggedIn, async (req, res) => {
   ]);
   res.render('user/add', { areas });
 });
+// Post
 router.post('/add', isLoggedIn, async (req, res) => {
   const areaName = req.body.area;
   let area = await pool.query(
@@ -25,22 +26,56 @@ router.post('/add', isLoggedIn, async (req, res) => {
   );
   area = JSON.parse(JSON.stringify(area))[0].id;
   const beds = await pool.query(
-    'SELECT * FROM beds WHERE id_area = ? AND status = "Free"',
+    'SELECT * FROM beds WHERE id_area = ? AND status = "Free" LIMIT 1',
     [area]
   );
+  if (beds.length == 1) {
+    const bed = JSON.parse(JSON.stringify(beds))[0];
 
-  let date_in = new Date(Date.now());
-  date_in = date_in.toString().split('GMT')[0];
+    let date_in = new Date(Date.now());
+    date_in = date_in.toString().split('GMT')[0];
 
-  const { cedula, name, eps, date_out } = req.body;
-  const newUser = {
-    cedula,
-    name,
-    eps,
-    date_in,
-    date_out
-  };
-  res.render('user/list');
+    const { cedula, name, eps, date_out } = req.body;
+    const newUser = {
+      cedula,
+      name,
+      eps,
+      date_in,
+      date_out,
+      bed: bed.id,
+      status: 'in'
+    };
+    console.log(newUser);
+    await pool.query('UPDATE beds set ? WHERE id = ?', [
+      { status: 'Busy' },
+      bed.id
+    ]);
+    await pool.query('INSERT INTO user set ?', [newUser]);
+    req.flash(
+      'success',
+      `${areaName} Bed ${bed.id} in room ${bed.location} assigned`
+    );
+    res.redirect('/user');
+  } else {
+    req.flash('message', `No beds available for ${areaName} area`);
+    res.redirect('/areas');
+  }
 });
+
+// dar de alta
+
+router.post('/:id', isLoggedIn, async (req, res) => {
+  const { id } = req.param;
+  let date_out = new Date(Date.now());
+  date_out = date_out.toString().split('GTM')[0];
+  const newUser = {
+    date_out,
+    status: 'out'
+  };
+
+  await pool.query('UPDATE user set ? WHERE id = ?', [newUser, id]);
+  res.redirect('/user/list', {});
+});
+
 // Module
 module.exports = router;
