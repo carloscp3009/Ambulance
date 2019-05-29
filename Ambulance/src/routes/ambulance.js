052f5094bf180903471c4b1a219166f8d8d182db
carlos <ccarlosa@uninorte.edu.co>
2019-05-29 14:53:29 -0500

    Route ok

--- Ambulance/src/routes/ambulance.js
+++ Ambulance/src/routes/ambulance.js
@@ -21,6 +21,18 @@ router.post('/ambulance', async (req, res) => {
 
 router.get('/ambulance/test/:id/:area', async (req, res) => {
   let { id, area } = req.params;
+
+  // Check availability
+  res.render('ambulance/coords', { area, id });
+});
+//
+///////
+
+////Master query
+
+//////
+router.post('/ambulance/test/:id/:area', async (req, res) => {
+  let { id, area } = req.params;
   // Get User
   let user = await poolEps.query('SELECT * FROM users WHERE id = ?', [id]);
   user = JSON.parse(JSON.stringify(user))[0];
@@ -39,27 +51,18 @@ router.get('/ambulance/test/:id/:area', async (req, res) => {
 
   // Get possibles hospital id Because EPS & Area & bed availbility
   let options = await pool.query(
-    'SELECT DISTINCT A.id_hospital, H.lat, H.long FROM Hospital.hospital AS H, Hospital.beds AS B, Hospital.areas AS A WHERE B.id_area IN (SELECT A.id FROM Hospital.hospital AS H, areas AS A WHERE H.hname in (?) AND A.name = ? AND A.id_hospital = H.idhospital) AND B.status ="Free" AND B.id_area = A.id AND H.idhospital = A.id_hospital',
+    'SELECT DISTINCT A.id_hospital FROM Hospital.hospital AS H, Hospital.beds AS B, Hospital.areas AS A WHERE B.id_area IN (SELECT A.id FROM Hospital.hospital AS H, areas AS A WHERE H.hname in (?) AND A.name = ? AND A.id_hospital = H.idhospital) AND B.status ="Free" AND B.id_area = A.id AND H.idhospital = A.id_hospital',
     [hnames, area]
   );
   options = JSON.parse(JSON.stringify(options));
 
   var ops = [];
   for (let i = 0; i < options.length; i++) {
-    ops[i] = options[i];
+    ops[i] = options[i].id_hospital;
   }
   console.log(ops);
-  // Check availability
-  res.render('ambulance/coords', { area, id });
-});
-//
-///////
 
-////Master query
-
-//////
-router.post('/ambulance/test/:id/:area', async (req, res) => {
-  let { id, area } = req.params;
+  /////////////////////////////////////////////////////////////////////////////////////
   let { origin } = req.body;
 
   origin = origin
@@ -70,11 +73,14 @@ router.post('/ambulance/test/:id/:area', async (req, res) => {
   let lon = origin[1];
 
   //console.log(origin[1]);
-  let sql = `SELECT H.hname,H.idhospital,H.lat,H.long,( 6371 * acos(cos(radians(${lat})) * cos(radians(lat)) * cos(radians(H.long) - radians(${lon})) + sin(radians(${lat})) * sin(radians(lat)))) AS distance FROM Hospital.hospital AS H`;
-  const pro = await pool.query(sql);
-  console.log(sql);
-  console.log(pro);
-  res.send(origin);
+  let sql = `SELECT H.hname,H.idhospital,H.lat,H.long,( 6371 * acos(cos(radians(${lat})) * cos(radians(lat)) * cos(radians(H.long) - radians(${lon})) + sin(radians(${lat})) * sin(radians(lat)))) AS distance FROM Hospital.hospital AS H WHERE H.idhospital IN (?) ORDER BY distance ASC`;
+  let hospitalSelected = await pool.query(sql, [ops]);
+
+  hospitalSelected = JSON.parse(JSON.stringify(hospitalSelected))[0];
+  const latf = hospitalSelected.lat;
+  const lonf = hospitalSelected.long;
+  //console.log(latLng);
+  res.render('ambulance/route', { lat, lon, latf, lonf });
 });
 // Modules Exports
 module.exports = router;
